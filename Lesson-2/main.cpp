ee84351714fc677a2bb77433c5129186d06802ef
IMZolin <Zolin5269@gmail.com>
2022-09-18 16:48:41 +0300

    Add Node and Tree classes

--- Lesson-2/main.cpp
+++ Lesson-2/main.cpp
@@ -1,9 +1,11 @@
 #include "Parser.h"
-
+#include "Tree.h"
 int main() {
     std::string filename = "C:\\Users\\zolin\\University\\Science_programming\\Lessons\\Lesson-2\\example.txt";
     Parser parser(filename);
+    Tree tree ("Root");
     parser.Parse();
+//    tree.Print(tree.getRoot());
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }

fd1c6a0819a4ad0f410b9c2191dacd5bd4e1c910
IMZolin <Zolin5269@gmail.com>
2022-09-18 16:43:19 +0300

    Add class Parser in 2-nd lab

--- Lesson-2/main.cpp
+++ Lesson-2/main.cpp
@@ -1,6 +1,9 @@
 #include "Parser.h"
 
 int main() {
+    std::string filename = "C:\\Users\\zolin\\University\\Science_programming\\Lessons\\Lesson-2\\example.txt";
+    Parser parser(filename);
+    parser.Parse();
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
